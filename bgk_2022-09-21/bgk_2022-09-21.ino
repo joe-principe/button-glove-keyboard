a72a9d4ed730df93ea6f898e9520f73a1bd574a9
Joe Principe <j.principe343@gmail.com>
2022-09-21 12:33:06 -0700

    Uncommented some lines

--- bgk_2022-09-21/bgk_2022-09-21.ino
+++ bgk_2022-09-21/bgk_2022-09-21.ino
@@ -1,9 +1,9 @@
 // Set the pin numbers of the pushbuttons
 // Will likely put these into an array later on
 const int LEFT_PINKY = 6;
-//const int LEFT_RING = 5;
-//const int LEFT_BIRD = 4;
-//const int LEFT_POINT = 3;
+const int LEFT_RING = 5;
+const int LEFT_BIRD = 4;
+const int LEFT_POINT = 3;
 const int LEFT_THUMB = 2;
 
 // Set the starting state of the buttons as low
@@ -15,9 +15,9 @@ void setup() {
 
   // Initialize the left hand pushbuttons as inputs
   pinMode(LEFT_PINKY, INPUT);
-//  pinMode(LEFT_RING, INPUT);
-//  pinMode(LEFT_BIRD, INPUT);
-//  pinMode(LEFT_POINT, INPUT);
+  pinMode(LEFT_RING, INPUT);
+  pinMode(LEFT_BIRD, INPUT);
+  pinMode(LEFT_POINT, INPUT);
   pinMode(LEFT_THUMB, INPUT);
 }
 
@@ -26,15 +26,15 @@ void loop() {
   l_pinky_buttonstate = digitalRead(LEFT_PINKY);
   Serial.print("Left Pinky Button State: ");
   Serial.println(l_pinky_buttonstate);
-//  l_ring_buttonstate = digitalRead(LEFT_RING);
-//  Serial.println("Left Ring Button State: ");
-//  Serial.println(l_ring_buttonstate);
-//  l_bird_buttonstate = digitalRead(LEFT_BIRD);
-//  Serial.println("Left Bird Button State: ");
-//  Serial.println(l_bird_buttonstate);
-//  l_point_buttonstate = digitalRead(LEFT_POINT);
-//  Serial.println("Left Pointer Button State: ");
-//  Serial.println(l_point_buttonstate);
+  l_ring_buttonstate = digitalRead(LEFT_RING);
+  Serial.println("Left Ring Button State: ");
+  Serial.println(l_ring_buttonstate);
+  l_bird_buttonstate = digitalRead(LEFT_BIRD);
+  Serial.println("Left Bird Button State: ");
+  Serial.println(l_bird_buttonstate);
+  l_point_buttonstate = digitalRead(LEFT_POINT);
+  Serial.println("Left Pointer Button State: ");
+  Serial.println(l_point_buttonstate);
   l_thumb_buttonstate = digitalRead(LEFT_THUMB);
   Serial.print("Left Thumb Button State: ");
   Serial.println(l_thumb_buttonstate);
